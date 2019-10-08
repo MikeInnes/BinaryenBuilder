4f7175ff63987db606cf48b7c57acd41596bc117
Mike J Innes <mike.j.innes@gmail.com>
2019-10-08 23:19:58 +0100

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -16,8 +16,9 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd binaryen/
+sed -i -e 's/Windows.h/windows.h/g' src/tools/wasm-reduce.cpp 
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain .
-make -j4
+make -j8
 make install
 
 """
@@ -34,21 +35,23 @@ platforms = [
     Linux(:x86_64, libc=:musl),
     Linux(:aarch64, libc=:musl),
     Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64)
+    MacOS(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64)
 ]
 
 # The products that we will ensure are always built
 products(prefix) = [
-    ExecutableProduct(prefix, "wasm-as", Symbol("wasm-as")),
+    ExecutableProduct(prefix, "wasm-as", :wasm_as),
     LibraryProduct(prefix, "libbinaryen", :libbinaryen),
-    ExecutableProduct(prefix, "wasm-opt", Symbol("wasm-opt")),
-    ExecutableProduct(prefix, "wasm-shell", Symbol("wasm-shell")),
+    ExecutableProduct(prefix, "wasm-opt", :wasm_opt),
+    ExecutableProduct(prefix, "wasm-shell", :wasm_shell),
     ExecutableProduct(prefix, "wasm2js", :wasm2js),
-    ExecutableProduct(prefix, "wasm-reduce", Symbol("wasm-reduce")),
-    ExecutableProduct(prefix, "wasm-metadce", Symbol("wasm-metadce")),
-    ExecutableProduct(prefix, "wasm-dis", Symbol("wasm-dis")),
-    ExecutableProduct(prefix, "wasm-ctor-eval", Symbol("wasm-ctor-eval")),
-    ExecutableProduct(prefix, "wasm-emscripten-finalize", Symbol("wasm-emscripten-finalize")),
+    ExecutableProduct(prefix, "wasm-reduce", :wasm_reduce),
+    ExecutableProduct(prefix, "wasm-metadce", :wasm_metadce),
+    ExecutableProduct(prefix, "wasm-dis", :wasm_dis),
+    ExecutableProduct(prefix, "wasm-ctor-eval", :wasm_ctor_eval),
+    ExecutableProduct(prefix, "wasm-emscripten-finalize", :wasm_emscripten_finalize),
     ExecutableProduct(prefix, "asm2wasm", :asm2wasm)
 ]
 
